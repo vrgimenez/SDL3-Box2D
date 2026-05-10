ab2854f389cfc2b5958fb1fd3b72eb635944f43d
V. Raúl Giménez <vrgimenez@gmail.com>
2026-05-10 14:30:49 -0300

    ci: implement clean automated release workflow with dynamic changelog

---
+++ version.h
@@ -0,0 +1,25 @@
+/* 
+ * File:   version.h
+ * Author: vrgimenez
+ *
+ * Created on 11 de octubre de 2018, 09:09
+ */
+
+#ifndef _VERSION_H
+#define _VERSION_H
+
+/*Type and constant definitions---------------------------------------------------------*/
+#define COMPANY_NAME_STR			"VRX Design"
+#define COMPANY_URL_STR				"github.com/vrgimenez"
+#define LEGAL_COPYRIGHT1_STR		"2026 Ing.V. Raúl Giménez | vrgimenez@gmail.com"
+#define LEGAL_TRADEMARKS_STR		"2026" COMPANY_NAME_STR " | " COMPANY_URL_STR
+#define PRODUCT_NAME_STR			"HelloSDL-B2D"
+#define PRODUCT_VERSION_STR			"26.0510"
+//#define PRODUCT_FULL_VERSION_STR	PRODUCT_NAME_STR " " PRODUCT_VERSION_STR
+
+/*External object declarations----------------------------------------------------------*/
+
+/*External function declarations--------------------------------------------------------*/
+
+#endif	/* _VERSION_H */
+
